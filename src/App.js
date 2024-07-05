dc920f1b90fec6821034aae200e19b64b215822a
new <new@Pranavs-Mac-mini.local>
2024-07-05 09:45:56 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import { LoginButton } from '@telegram-auth/react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <LoginButton
+                botUsername='@Tet1234bot'
+                onAuthCallback={(data) => {
+                    console.log(data);
+                    // call your backend here to validate the data and sign in the user
+                }}
+            />
     </div>
   );
 }
